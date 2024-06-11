c7660eedd78707917e70b26b1f426d23c957f70c
proti <bernardo.proti@hotmail.com>
2024-06-11 16:52:34 -0300

    add: userLogin(), authenticateUser() and errorMessage() functions were created.

---
+++ script.js
@@ -0,0 +1,56 @@
+const users = {
+    "registeredUsers": {
+        "admin": {
+            "login": "admin@fortes.ind.br",
+            "password": "admin"    
+        },
+        "user1": {
+            "login": "user1@gmail.com",
+            "password": "user1"
+        }
+    }
+};
+
+function errorMessage(message) {
+    let errorBox = document.getElementById("errorBox");
+    let errorMessage = errorBox.querySelector("p");
+
+    errorMessage.innerText = message;
+    errorBox.style.display = "block";
+
+    userMail.style.borderColor = "#DB0000";
+    userPass.style.borderColor = "#DB0000";
+}
+
+function authenticateUser(inputMail, inputPass) {
+    const registeredUsers = users.registeredUsers;
+
+    for (let user in registeredUsers) {
+        if (registeredUsers[user].login === inputMail && registeredUsers[user].password === inputPass) {
+            return true;
+        }
+    }
+    
+    return false;
+}
+
+function userLogin(event) {
+    event.preventDefault();
+
+    let userMail = document.getElementById("userMail");
+    let userPass = document.getElementById("userPass");
+    
+    // Verificação de possíveis erros.
+    if (userMail.value == "" && userPass.value == "") {
+        errorMessage("Você não preencheu os campos de e-mail e senha.");
+    } else if (userMail.value == "" || userPass.value == "") {
+        errorMessage("Você não preencheu todos os campos necessários.");
+    } else {
+        // Autenticação de entrada.
+        if (authenticateUser(userMail.value, userPass.value)) {
+            window.alert("Login realizado com sucesso.");
+        } else {
+            window.alert("Login fracassou.")
+        }
+    }
+}
